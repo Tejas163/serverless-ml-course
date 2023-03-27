788f2ff063c147b8dfede72bd12eb98154fcaa29
Tejaswi SK <32892292+Tejas163@users.noreply.github.com>
2023-03-27 19:55:27 +0530

    Update run-feature-and-prediction-pipelines.sh

--- src/01-module/scripts/run-feature-and-prediction-pipelines.sh
+++ src/01-module/scripts/run-feature-and-prediction-pipelines.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 set -e
-jupyter nbconvert --to notebook --execute iris-feature-pipeline.ipynb
-jupyter nbconvert --to notebook --execute iris-batch-inference-pipeline.ipynb
+jupyter nbconvert --to notebook --execute iris_feature_pipeline.ipynb
+jupyter nbconvert --to notebook --execute iris_batch_inference_pipeline.ipynb
 
